276942e274f568981d448f8f0eb03fc37e4dd3a4
saeed <me.coderr@gmail.com>
2024-04-04 10:41:02 +0530

    designing left, api calls to get tasks done

--- frontend/src/Components/Task.tsx
+++ frontend/src/Components/Task.tsx
@@ -13,25 +13,25 @@ const Task = () => {
 
   const {openTask, setOpenTask} = TaskState();
 
-    const fetchTasks = async () => {
-      try {
-        const response = await fetch(`${SERVER_URL}/tasks/`, {
-          method: 'GET',
-          body: JSON.stringify({_id: openTask?._id})
-        });
-        const data = await response.json();
+    // const fetchTasks = async () => {
+    //   try {
+    //     const response = await fetch(`${SERVER_URL}/tasks/`, {
+    //       method: 'GET',
+    //       body: JSON.stringify({_id: openTask?._id})
+    //     });
+    //     const data = await response.json();
 
-      } catch(err) {
-        if(err instanceof Error) {
-          console.log(err.message);
-        } else {
-          console.log(`Unknown error found`);
-        }
-      }
-    }
-    useEffect(() => {
-        fetchTasks();
-    }, [])
+    //   } catch(err) {
+    //     if(err instanceof Error) {
+    //       console.log(err.message);
+    //     } else {
+    //       console.log(`Unknown error found`);
+    //     }
+    //   }
+    // }
+    // useEffect(() => {
+    //     fetchTasks();
+    // }, [])
 
   return (
     <div onClick={() => setOpenTask(null)}>
